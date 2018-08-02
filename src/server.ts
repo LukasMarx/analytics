3f15f15afeed1a03826d82b26331d1ecffa325a5
Lukas Marx <marxlukas@googlemail.com>
2018-08-02 12:00:54 +0200

    first cutsom ws impl

--- src/server.ts
+++ src/server.ts
@@ -1,74 +1,78 @@
-import express from 'express';
-import http from 'http';
-import socketIO, { Socket } from 'socket.io';
-import cors from 'cors';
 import * as mongo from 'mongodb';
 import { Subject } from 'rxjs';
+import { Server } from 'ws';
+import { v4 } from 'uuid';
 
-const uri = process.env.MONGODB;
+// const uri = process.env.MONGODB;
 
-let cachedDb: mongo.Db = undefined;
+// let cachedDb: mongo.Db = undefined;
 
-mongo.MongoClient.connect(
-  uri,
-  { poolSize: 10 }
-)
-  .then(async client => {
-    cachedDb = client.db('analytics');
-    const result = await cachedDb.command({ listCollections: 1 });
-    try {
-      cachedDb.createCollection('logs', { capped: true, size: 1073741824 });
-    } catch (error) {}
-    cachedDb.collection('logs').createIndex({ projectId: 1 }, { background: true, unique: false });
-  })
-  .catch();
+// mongo.MongoClient.connect(
+//   uri,
+//   { poolSize: 10 }
+// )
+//   .then(async client => {
+//     cachedDb = client.db('analytics');
+//     const result = await cachedDb.command({ listCollections: 1 });
+//     try {
+//       cachedDb.createCollection('logs', { capped: true, size: 1073741824 });
+//     } catch (error) {}
+//     cachedDb.collection('logs').createIndex({ projectId: 1 }, { background: true, unique: false });
+//   })
+//   .catch();
 
-const app = express();
-app.use(cors());
-const httpServer = (<any>http).Server(app);
-const io = socketIO(httpServer, { path: '/analytics', origins: '*:*', cookie: false });
+const server = new Server({ port: 3001, path: '/analytics' });
 
 const userCount = new Subject<number>();
 const currentlyReading = new Subject<{ path: string; socketId: string }>();
 const whoSeesWhat: { [key: string]: string } = {};
 
-let counter = 0;
-io.on('connection', function(socket) {
-  socket.on('navigation', msg => {
-    const dataset = { projectId: msg.projectId, path: msg.path, timestamp: new Date().getTime() };
-    whoSeesWhat[socket.id] = msg.path;
-    currentlyReading.next({ path: (<any>msg).path, socketId: socket.id });
-    cachedDb.collection('logs').insert(dataset);
-  });
+const adminRoom: { [key: string]: any } = {};
+const userRoom: { [key: string]: any } = {};
 
-  console.log('connected ' + socket.id);
-  counter++;
-  console.log(counter + ' active connections');
-
-  socket.once('disconnect', () => {
-    console.log('disconnected ' + socket.id);
-    counter--;
-    console.log(counter + ' active connections');
-    delete whoSeesWhat[socket.id];
-    currentlyReading.next();
-    socket.disconnect(true);
+server.on('connection', connection => {
+  (<any>connection).isAlive = true;
+  (<any>connection).id = v4();
+  connection.on('message', message => {
+    handleMessage(connection, message);
   });
-  socket.on('error', function(err) {
-    console.log('socket error: ' + err);
-    socket.disconnect(true);
+  connection.on('close', () => {
+    delete adminRoom[(<any>connection).id];
+    delete userRoom[(<any>connection).id];
+    for (const key in adminRoom) {
+      adminRoom[key].send(JSON.stringify({ messageType: 'usercount', payload: Object.keys(userRoom).length }));
+    }
   });
+  connection.on('pong', () => ((<any>connection).isAlive = true));
 });
 
-io.of('/admin').on('connection', function(socket) {
-  const sub1 = currentlyReading.subscribe(reader => {
-    socket.emit('usercount', Object.keys(whoSeesWhat).length);
-    socket.emit('reading', whoSeesWhat);
-  });
-  socket.on('disconnect', () => {
-    sub1.unsubscribe();
-  });
-});
+function handleMessage(connection: any, msg: any) {
+  const message = JSON.parse(msg);
 
-httpServer.listen(3000, function() {
-  console.log('listening on *:3000');
-});
+  switch (message.messageType) {
+    case 'joinAdmin': {
+      adminRoom[connection.id] = connection;
+      break;
+    }
+    case 'joinUser': {
+      userRoom[connection.id] = connection;
+      for (const key in adminRoom) {
+        adminRoom[key].send(JSON.stringify({ messageType: 'usercount', payload: Object.keys(userRoom).length }));
+      }
+      break;
+    }
+    case 'navigation': {
+      break;
+    }
+  }
+}
+
+const interval = setInterval(() => {
+  server.clients.forEach(client => {
+    const c: any = client;
+    if (c.isAlive === false) return client.terminate();
+
+    c.isAlive = false;
+    client.ping(() => {});
+  });
+}, 10000);
