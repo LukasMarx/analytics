6813e876f8c70ccd8c5d4db952dad73a0a861d9a
Lukas Marx <marxlukas@googlemail.com>
2018-08-05 14:11:47 +0200

    notlösung

--- src/server.ts
+++ src/server.ts
@@ -1,92 +1,18 @@
-import * as mongo from 'mongodb';
-import { Subject } from 'rxjs';
-import { Server } from 'ws';
-import { v4 } from 'uuid';
+import express from 'express';
+import http from 'http';
+import socketIO, { Socket } from 'socket.io';
+import cors from 'cors';
 
-// const uri = process.env.MONGODB;
+const app = express();
+app.use(cors());
+const httpServer = (<any>http).Server(app);
+const io = socketIO(httpServer, { path: '/analytics', origins: '*:*', cookie: false });
 
-// let cachedDb: mongo.Db = undefined;
-
-// mongo.MongoClient.connect(
-//   uri,
-//   { poolSize: 10 }
-// )
-//   .then(async client => {
-//     cachedDb = client.db('analytics');
-//     const result = await cachedDb.command({ listCollections: 1 });
-//     try {
-//       cachedDb.createCollection('logs', { capped: true, size: 1073741824 });
-//     } catch (error) {}
-//     cachedDb.collection('logs').createIndex({ projectId: 1 }, { background: true, unique: false });
-//   })
-//   .catch();
-
-const server = new Server({ port: 3000, path: '/analytics' });
-
-const userCount = new Subject<number>();
-const currentlyReading = new Subject<{ path: string; socketId: string }>();
-const whoSeesWhat: { [key: string]: string } = {};
-
-const adminRoom: { [key: string]: any } = {};
-const userRoom: { [key: string]: any } = {};
-
-server.on('connection', (connection, req) => {
-  (<any>connection).isAlive = true;
-  (<any>connection).id = v4();
-  connection.on('message', message => {
-    handleMessage(connection, message);
-  });
-  connection.on('close', () => {
-    delete adminRoom[(<any>connection).id];
-    delete userRoom[(<any>connection).id];
-    for (const key in adminRoom) {
-      adminRoom[key].send(JSON.stringify({ messageType: 'usercount', payload: Object.keys(userRoom).length }));
-    }
-  });
-  connection.on('pong', () => ((<any>connection).isAlive = true));
-
-  console.log(JSON.stringify(req.rawHeaders));
-  console.log('--- all connected users ---');
-  for (const key in userRoom) {
-    console.log(userRoom[key].id, userRoom[key].readyState);
-  }
+let counter = 0;
+io.on('connection', function(socket) {
+  socket.disconnect(true);
 });
 
-function handleMessage(connection: any, msg: any) {
-  const message = JSON.parse(msg);
-
-  switch (message.messageType) {
-    case 'joinAdmin': {
-      adminRoom[connection.id] = connection;
-      break;
-    }
-    case 'joinUser': {
-      userRoom[connection.id] = connection;
-      for (const key in adminRoom) {
-        adminRoom[key].send(JSON.stringify({ messageType: 'usercount', payload: Object.keys(userRoom).length }));
-      }
-      break;
-    }
-    case 'navigation': {
-      break;
-    }
-  }
-}
-
-const interval = setInterval(() => {
-  server.clients.forEach(client => {
-    const c: any = client;
-    if (c.isAlive === false) {
-      delete adminRoom[c.id];
-      delete userRoom[c.id];
-      console.log('Connection ' + c.id + ' timed out');
-      client.terminate();
-      return;
-    }
-
-    c.isAlive = false;
-    client.ping(null, null, error => {
-      if (error) console.log(error);
-    });
-  });
-}, 10000);
+httpServer.listen(3000, function() {
+  console.log('listening on *:3000');
+});
